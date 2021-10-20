10f93bfd591dd87af493ebe9a94f1127f9cd541c
Zack Johnson <zackmj99@gmail.com>
2021-10-20 09:13:05 -0500

    delete config.py

--- olympics/config.py
+++
@@ -1,9 +0,0 @@
-''' Config.py
-    By: Zack Johnson, October 18, 2021
-
-    Config file for accessing the olympics database. For use with olympics.py.
-'''
-
-database = 'olympics'
-user = 'zackamo'
-password = 'psqlword'
